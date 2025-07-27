6e6ad451e94efb1e9ca3dba5a1f714c2c607bca2
Mohamed99-Ahmed <mohamed202029999@gmail.com>
2025-07-27 06:02:09 +0300

    deleted some file to fix in vercel

--- utils/imagekit.js
+++
@@ -1,9 +0,0 @@
-const ImageKit = require("imagekit");
-
-const imagekit = new ImageKit({
-  publicKey: "public_pJ90T0jCpU5Dn7tq8Xkq2/3i2ew=",
-  privateKey: "private_HgwoLFH3IYdBY9A8+3QnO//WY0c=",
-  urlEndpoint: "https://ik.imagekit.io/MohamedAhmed/codeny/", // رابط مكتبة الصور
-});
-
-module.exports = imagekit;
